0a2240f83510fe533f6efe8e9af51ad95bc7f2cc
giadavolpin <volpingiada@gmail.com>
2022-12-19 16:06:03 +0100

    db

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        /*      $books = ['autore' => 'autore 1'];
+        return view('home', compact('books'));
+        */
+
+
+
+    }
+}
