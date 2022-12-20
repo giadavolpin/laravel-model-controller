e3107175ff11f9c0a31e2a09eac1e70d5924c17a
giadavolpin <volpingiada@gmail.com>
2022-12-20 19:31:26 +0100

    mi

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        // nella index si visualizza sempre tutto il contenuto di una tabella
+
+        // prendo tutti i film dalla tabella del database
+        $movies = Movie::all();
+
+        // richiamo la view "home" e le passo i film che ho preso prima
+        return view('home', compact("movies"));
+    }
+    public function getfilm()
+    {
+        // $movies = Movie::all();
+        // return view('home', compact('movies'));
+    }
+
+/*      $books = ['autore' => 'autore 1'];
+return view('home', compact('books'));
+*/
+
+
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,28 @@
+<<<<<<< HEAD
 @extends('layout.app')
 	@section('content')
 		<h1> prova
 	@endsection
+=======
+@extends('layouts.app');
+
+@section('content')
+	<section class="container">
+		<ul>
+			@foreach ($movies as $movie)
+				<li>{{$movie->title}}{{$movie->date}}</li> 
+			@endforeach
+		</ul>
+	</section>
+@endsection
+
+{{-- 
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> --}}
+>>>>>>> e7fb1cd381c749f4cd1813226d3cc843b99f0b4b
